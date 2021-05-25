88f1807859c58c793ac6cdf3fc6eb39d99305a8f
mitsuoyg <yshara.mitsuo@gmail.com>
2021-05-25 17:33:08 -0500

    feat: title Mibot

--- nuxt.config.js
+++ nuxt.config.js
@@ -9,8 +9,8 @@ export default {
   },
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    titleTemplate: '%s - mibot',
-    title: 'mibot',
+    titleTemplate: '%s',
+    title: 'Mibot',
     htmlAttrs: {
       lang: 'es'
     },
